fa4eb6274be23d6b27fcc2f0a934824a2f8ecb41
JuanDenis <juanfedericodenis@gmail.com>
2021-08-20 01:42:17 -0300

    OAuth Google

--- js/desconectar.js
+++ js/desconectar.js
@@ -1,4 +1,18 @@
 function desconectar(){
     localStorage.clear();
     location.href = "login.html";
+    signOut();
 }
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    location.reload();
+    });
+  }
+  
+function onload() {
+    gapi.load('auth2', function() {
+      gapi.auth2.init();
+    });
+  }
--- js/login.js
+++ js/login.js
@@ -16,9 +16,25 @@ function verificar(){
         localStorage.setItem('usuario',JSON.stringify(usuario));
     }   
 }
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId());
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+  location.href = "index.html";
+  
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+}
+
 document.addEventListener("DOMContentLoaded", ()=>{
    let usuario = JSON.parse(localStorage.getItem('usuario'));
    if (usuario.estado== 'conectado'){
        location.href = 'index.html';
    }
 });
+
+  
